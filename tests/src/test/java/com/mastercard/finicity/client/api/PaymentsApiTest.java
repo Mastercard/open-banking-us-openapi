0d46996cac9483d4c280c076d123380ede7b9542
Jean-Alexis Aufauvre <jean-alexis.aufauvre@mastercard.com>
2022-04-11 19:32:05 +0100

    Improved "Payments" endpoints, renamed some schemas

---
+++ tests/src/test/java/com/mastercard/finicity/client/api/PaymentsApiTest.java
@@ -0,0 +1,62 @@
+package com.mastercard.finicity.client.api;
+
+import com.mastercard.finicity.client.ApiException;
+import com.mastercard.finicity.client.model.CustomerAccount;
+import com.mastercard.finicity.client.test.BaseTest;
+import com.mastercard.finicity.client.test.utils.AccountUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PaymentsApiTest extends BaseTest {
+
+    private final PaymentsApi api = new PaymentsApi(apiClient);
+
+    private static CustomerAccount existingAccount;
+
+    @BeforeAll
+    static void beforeAll() {
+        try {
+            var accountApi = new AccountsApi(apiClient);
+            existingAccount = AccountUtils.getCustomerAccounts(accountApi, CUSTOMER_ID, "savings").get(0);
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+
+    @Test
+    public void getAccountACHDetailsTest() {
+        try {
+            var achDetails = api.getAccountACHDetails(CUSTOMER_ID, existingAccount.getId());
+            assertNotNull(achDetails.getRealAccountNumber());
+            assertNotNull(achDetails.getRoutingNumber());
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+
+    @Test
+    public void getAvailableBalanceTest() {
+        try {
+            var balance = api.getAvailableBalance(CUSTOMER_ID, existingAccount.getId());
+            assertNotNull(balance.getAvailableBalance());
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+
+    @Test
+    public void getAvailableBalanceLiveTest() {
+        try {
+            var balance = api.getAvailableBalanceLive(CUSTOMER_ID, existingAccount.getId());
+            assertNotNull(balance.getAvailableBalance());
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+}
