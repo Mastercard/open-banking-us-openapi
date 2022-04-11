e96bbebbef2e79753ee9e18909b27246a4cae8d3
Jean-Alexis Aufauvre <jean-alexis.aufauvre@mastercard.com>
2022-04-11 12:21:33 +0100

    Improved "App Registration" endpoints, added tests, clean-up

---
+++ tests/src/test/java/com/mastercard/finicity/client/api/AppRegistrationApiTest.java
@@ -0,0 +1,83 @@
+package com.mastercard.finicity.client.api;
+
+import com.mastercard.finicity.client.ApiException;
+import com.mastercard.finicity.client.test.AccountUtils;
+import com.mastercard.finicity.client.test.BaseAppKeyAppTokenTest;
+import com.mastercard.finicity.client.test.ModelFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppRegistrationApiTest extends BaseAppKeyAppTokenTest {
+
+    private final AppRegistrationApi api = new AppRegistrationApi();
+    private final static AccountsApi accountApi = new AccountsApi(apiClient);
+
+    @Test
+    void registerAppTest() {
+        try {
+            var application = ModelFactory.newApplication();
+            var registeredApplication = api.registerApp(application);
+            assertNotNull(registeredApplication.getPreAppId());
+            assertEquals("P", registeredApplication.getStatus());
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+
+    @Test
+    void getAppRegistrationStatusV2Test() {
+        try {
+            var statuses = api.getAppRegistrationStatusV2(null, null, null, null, null, null, null, null);
+            assertNotNull(statuses.getNumberOfRecordsPerPage());
+            assertNotNull(statuses.getPageNumber());
+            assertNotNull(statuses.getTotalPages());
+            assertNotNull(statuses.getTotalRecords());
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+
+    @Test
+    void modifyAppRegistrationTest() {
+        try {
+            var application = ModelFactory.newApplication();
+            var preAppId = api.registerApp(application).getPreAppId();
+            application.setAppDescription("Updated description");
+            var updatedApplication = api.modifyAppRegistration(preAppId, application);
+            assertEquals(preAppId, updatedApplication.getPreAppId());
+            assertEquals("P", updatedApplication.getStatus());
+        } catch (ApiException e) {
+            logApiException(e);
+            fail();
+        }
+    }
+
+    @Test
+    void migrateInstitutionLoginAccountsV2Test() {
+        try {
+            var existingAccount = AccountUtils.getCustomerAccounts(accountApi, CUSTOMER_ID).get(0);
+            var existingInstitutionLoginId = existingAccount.getInstitutionLoginId();
+            var accounts = api.migrateInstitutionLoginAccountsV2(CUSTOMER_ID, existingInstitutionLoginId, new Object());
+            assertTrue(accounts.getAccounts().size() > 0);
+            fail();
+        } catch (ApiException e) {
+            // {"code":20007,"message":"Accounts on institution XXXXXX cannot be migrated "}
+            logApiException(e);
+        }
+    }
+
+    @Test
+    void setCustomerAppIDTest() {
+        try {
+            api.setCustomerAppID(CUSTOMER_ID, "1234", new Object());
+        } catch (ApiException e) {
+            // {"code":10007,"message":"invalid application id"}
+            logApiException(e);
+            assertErrorCodeEquals(10007, e);
+            assertErrorMessageEquals("invalid application id", e);
+        }
+    }
+}
--- tests/src/test/java/com/mastercard/finicity/client/test/ModelFactory.java
+++ tests/src/test/java/com/mastercard/finicity/client/test/ModelFactory.java
@@ -38,6 +38,21 @@ public final class ModelFactory {
                 .birthday(new Birthday().year(1989).month(8).dayOfMonth(13));
     }
 
+    public static Application newApplication() {
+        return new Application()
+                .appDescription("The app that makes your budgeting experience awesome")
+                .appName("AwesomeBudgetApp_" + randomStr())
+                .appUrl("https://www.finicity.com/")
+                .ownerAddressLine1("434 W Ascension Way")
+                .ownerAddressLine2("Suite #200")
+                .ownerName("Finicity")
+                .ownerPostalCode("84123")
+                .ownerState("UT")
+                .image("PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+CjxzdmcgICAKICAgeG1sbnM6c3ZnPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIKICAgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIgogICB2ZXJzaW9uPSIxLjEiCiAgIHZpZXdCb3g9IjAgMCAwIDAiCiAgIGhlaWdodD0iMCIKICAgd2lkdGg9IjAiPgogICAgPGcvPgo8L3N2Zz4K")
+                .ownerCity("Murray")
+                .ownerCountry("USA");
+    }
+
     public static String randomStr() {
         return RandomStringUtils.randomAlphabetic(10);
     }
