ca7edf60f70ab50a02207e11910098ddc48e7665
Jean-Alexis Aufauvre <jean-alexis.aufauvre@mastercard.com>
2022-04-11 12:21:24 +0100

    Improved "Customer" endpoints, declared all tags, renamed some tags, clean-up

--- tests/src/test/java/com/mastercard/finicity/client/test/ModelFactory.java
+++ tests/src/test/java/com/mastercard/finicity/client/test/ModelFactory.java
@@ -3,6 +3,8 @@ package com.mastercard.finicity.client.test;
 import com.mastercard.finicity.client.model.Borrower;
 import com.mastercard.finicity.client.model.BorrowerType;
 import com.mastercard.finicity.client.model.ConsumerInfo;
+import com.mastercard.finicity.client.model.NewCustomer;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public final class ModelFactory {
 
@@ -15,5 +17,17 @@ public final class ModelFactory {
                     .ssn("999999999")
                     .dob(470275200L)
             );
+    }
+
+    public static NewCustomer newCustomer() {
+        var username = "customer_" + randomStr();
+        return new NewCustomer()
+                .firstName("John_" + randomStr())
+                .lastName("Smith_" + randomStr())
+                .username(username);
+    }
+
+    public static String randomStr() {
+        return RandomStringUtils.randomAlphabetic(10);
     }
 }
