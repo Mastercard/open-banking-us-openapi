0aed752fba31f7146e5c5962609ded56c2c5c810
Jean-Alexis Aufauvre <jean-alexis.aufauvre@mastercard.com>
2022-05-19 16:52:53 +0100

    Fixed GenerateConsumerAttributesRequest (tests)

--- tests/src/test/java/com/mastercard/finicity/client/api/AnalyticsAndAttributesApiTest.java
+++ tests/src/test/java/com/mastercard/finicity/client/api/AnalyticsAndAttributesApiTest.java
@@ -3,21 +3,34 @@ package com.mastercard.finicity.client.api;
 import com.mastercard.finicity.client.ApiException;
 import com.mastercard.finicity.client.model.ConsumerAttributeAccountIDs;
 import com.mastercard.finicity.client.test.BaseTest;
+import com.mastercard.finicity.client.test.utils.AccountUtils;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AnalyticsAndAttributesApiTest extends BaseTest {
 
+    private static String existingAccountId;
     private final AnalyticsAndAttributesApi api = new AnalyticsAndAttributesApi(apiClient);
 
+    @BeforeAll
+    protected static void beforeAll() {
+        try {
+            // Load existing IDs to be used in the subsequent tests
+            var existingAccount = AccountUtils.getCustomerAccounts(new AccountsApi(apiClient), CUSTOMER_ID).get(0);
+            existingAccountId = existingAccount.getId();
+        } catch (ApiException e) {
+            fail();
+            logApiException(e);
+        }
+    }
 
     @Test
     void generateConsumerAttributesTest() {
         try {
-            var attributes = new ArrayList<ConsumerAttributeAccountIDs>();
+            var attributes = new ConsumerAttributeAccountIDs()
+                    .addAccountIdsItem(existingAccountId);
             api.generateConsumerAttributes(CUSTOMER_ID, attributes);
         } catch (ApiException e) {
             // {"detail":"CA360 System Error"}
@@ -28,7 +41,8 @@ public class AnalyticsAndAttributesApiTest extends BaseTest {
     @Test
     public void generateFCRAConsumerAttributesTest() {
         try {
-            var attributes = new ArrayList<ConsumerAttributeAccountIDs>();
+            var attributes = new ConsumerAttributeAccountIDs()
+                    .addAccountIdsItem(existingAccountId);
             api.generateFCRAConsumerAttributes(CUSTOMER_ID, attributes);
         } catch (ApiException e) {
             // {"detail":"CA360 System Error"}
