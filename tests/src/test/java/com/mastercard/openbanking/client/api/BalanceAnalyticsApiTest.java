4d1d42ddfb4026c26d39c97ab34588164ba48b0e
Padma Jadav <147383171+Jadavpadma@users.noreply.github.com>
2025-06-16 14:33:01 +0530

    Fixed failure BalanceAnalyticsApiTest.java

--- tests/src/test/java/com/mastercard/openbanking/client/api/BalanceAnalyticsApiTest.java
+++ tests/src/test/java/com/mastercard/openbanking/client/api/BalanceAnalyticsApiTest.java
@@ -12,23 +12,23 @@ class BalanceAnalyticsApiTest extends BaseTest {
 
     private final CustomBalanceAnalyticsApi api = new CustomBalanceAnalyticsApi(apiClient);
 
-    // @Test
-    // void generateBalanceAnalyticsTest() {
-    //     try {
-    //         // Generate a report
-    //         var reportConstraints = new BalanceAndCashFlowAnalyticsReportConstraints();
-    //         var generateResponse = api.generateBalanceAnalytics(CUSTOMER_ID, reportConstraints, null);
-    //         var reportId = generateResponse.getReportId();
+    @Test
+    void generateBalanceAnalyticsTest() {
+        try {
+            // Generate a report
+            var reportConstraints = new BalanceAndCashFlowAnalyticsReportConstraints();
+            var generateResponse = api.generateBalanceAnalytics(CUSTOMER_ID, reportConstraints, null);
+            var reportId = generateResponse.getReportId();
 
-    //         // Fetch the report as JSON
-    //         var report = api.getObbAnalyticsJsonReport(reportId);
-    //         assertNotNull(report);
+            // Fetch the report as JSON
+            var report = api.getObbAnalyticsJsonReport(reportId);
+            assertNotNull(report);
 
-    //         // Fetch the report as PDF
-    //         var pdf = api.getObbAnalyticsPdfReport(reportId);
-    //         assertNotNull(pdf);
-    //     } catch (ApiException e) {
-    //         fail(e);
-    //     }
-    // }
+            // Fetch the report as PDF
+            var pdf = api.getObbAnalyticsPdfReport(reportId);
+            assertNotNull(pdf);
+        } catch (ApiException e) {
+            fail(e);
+        }
+    }
 }
